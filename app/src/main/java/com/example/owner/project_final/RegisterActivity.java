497976fa6bbe0a822e2cb3ab18eae2d525f0b56a
kasohyun <40164480+kasohyun@users.noreply.github.com>
2019-03-11 13:50:43 +0900

    login

--- app/src/main/java/com/example/owner/project_final/RegisterActivity.java
+++ app/src/main/java/com/example/owner/project_final/RegisterActivity.java
@@ -83,51 +83,5 @@ public class RegisterActivity extends LoginActivity {
             }
         });
 
-/*
-        fd = FirebaseDatabase.getInstance();
-        fdRef = fd.getReference("users");
-        // CRUD 작업의 기준이 되는 노드를 레퍼런스로 가져옵니다.
-        // users 이하의 노드들을 가리킴
-
-        .setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String key = fdRef.push().getKey(); // 키를생성하고 키 값을 가져온다.
-
-                String name = nameText.getText().toString();
-                String birth = birthText.getText().toString();
-                String id = idText.getText().toString();
-                String password = passwordText.getText().toString();
-                String cname = cnameText.getText().toString();
-                String address = addressText.getText().toString();
-                String number = numberText.getText().toString();
-
-                String auth = authText.getText().toString();
-                Map<String, String > postValues = new HashMap<>();
-                // Map<key, value> 형태로 저장합니다.
-                // Map 인터페이스의 한 종류로 ( "Key", value) 로 이뤄져 있다
-                // key 값을 중복이 불가능 하고 value는 중복이 가능. value에 null값도 사용 가능하다.
-                // 멀티쓰레드에서 동시에 HashMap을 건드려 Key - value값을 사용하면 문제가 될 수 있다. 멀티쓰레드에서는 HashTable을 쓴다
-                postValues.put("name", name);
-                postValues.put("birth", birth);
-                postValues.put("id", id);
-                postValues.put("password", password);
-                postValues.put("cname", cname);
-                postValues.put("address", address);
-                postValues.put("number", number);
-
-                // postValues.put("auth",auth); 인증번호 - SMTP사용
-                DatabaseReference keyRef = fdRef.child(key);
-                // fdRef의 자식 키값을 가리킨다.
-                keyRef.setValue(postValues);
-                // 클래스
-                Toast.makeText(getApplicationContext(), "가입완료.",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class );
-                startActivity(intent);
-                finish();
-            }
-        });
-*/
-
     }
 }
